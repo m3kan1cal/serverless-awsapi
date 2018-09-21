134ee6dfb90f5cde0a31502fa8db349bb3575799
mlfowler-mh <mlfowler@magellanhealth.com>
2018-09-20 22:26:46 -0400

    Adding logging to create handler

--- functions/handlers/create.py
+++ functions/handlers/create.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 import boto3
 
@@ -8,11 +9,26 @@ from functions.beacon import respond
 from functions.models.note import NoteModel
 
 
+# Set up logging levels and constructs.
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logging.getLogger("boto").setLevel(logging.ERROR)
+logging.getLogger("botocore").setLevel(logging.ERROR)
+
+hdlr = logging.StreamHandler()
+hdlr.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+
+
 def create(event, context):
     """Create item in the collection."""
 
     try:
-        
+
         # Determine if required env var for region is present.
         val.check_region()
 
@@ -32,7 +48,7 @@ def create(event, context):
         # *aaS provider resources away from biz logic.
         region = os.environ["AWS_DEFAULT_REGION"]
         table = os.environ["DYNAMODB_TABLE"]
-        
+
         # Determine which DynamoDB host we need (local/remote)?
         host = val.check_dynamodb_host()
         conn = boto3.resource("dynamodb", region, endpoint_url=host)
@@ -42,22 +58,29 @@ def create(event, context):
         note = NoteModel(conn_table)
         item = note.save(data["userId"], data["notebook"], data["text"])
 
+        logger.info("Note created: {}".format(item))
         return respond(201, item)
-    
+
     except ex.AwsRegionNotSetException as exc:
+        logger.error(str(exc))
         return respond(500, {"error": str(exc)})
 
     except ex.DynamoDbTableNotSetException as exc:
+        logger.error(str(exc))
         return respond(500, {"error": str(exc)})
 
     except ex.RequestBodyNotSetException as exc:
+        logger.error(str(exc))
         return respond(400, {"error": str(exc)})
-        
+
     except ex.RequestBodyNotJsonException as exc:
+        logger.error(str(exc))
         return respond(400, {"error": str(exc)})
 
     except ex.RequiredPropertiesNotSetException as exc:
+        logger.error(str(exc))
         return respond(400, {"error": str(exc)})
 
     except Exception as exc:
+        logger.error(str(exc))
         return respond(500, {"error": str(exc)})
